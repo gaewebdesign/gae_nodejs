5da0e754e09c3d16eb38b1da01af4db062bdc1e1
githubmutt@gmail.com <github.mutt@gmail.com>
2023-01-11 02:15:25 -0800

    angular

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+
+//const mongoose = require('mongoose')
+
+
+
 const app = express();
 
+app.use(express.static("./public"))
+
+
+/*
 app.get('/', (req, res) => {
   res.send('Hello from App Engine! Wed 12:46am');
 });
+*/
+
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening in on port ${PORT}...`);
+
+
 });
